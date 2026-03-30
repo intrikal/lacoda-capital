da8d8c09ad7b42fe5c8cf20802d4632aa6d301e4
Kevin Lam <retrobytetech@gmail.com>
2026-03-29 19:10:05 -0700

    Add Firefox and WebKit to Playwright browser matrix

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,6 +19,14 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
   ],
 
   // Start the Next.js dev server before running tests
